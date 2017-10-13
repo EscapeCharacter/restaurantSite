4dacfd35aae98c33a6fd24792653c0b6481c3a30
Sagiv Lapkin <lapkins@USH10024.local>
2017-10-13 12:00:48 -0400

    First commit.

---
+++ Student/includes/header.php
@@ -0,0 +1,30 @@
+<?php
+	$companyName = "Frankin's Fine Dining"; 
+	include('includes/arrays.php');
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title><?php echo TITLE; ?></title>
+	<link rel="stylesheet" href="../../assets/styles.css">
+</head>
+
+<body id="final-example">
+
+	<div class="wrapper">
+		
+		<div id="banner">
+			<a href="/" title="Return to Home">
+				<img src="img/banner.png" alt="Franklin's Fine Dining">
+			</a>
+		</div>
+		
+		<div id="nav">
+				<?php include('includes/nav.php'); ?>
+		</div>
+
+		<div class="content">
